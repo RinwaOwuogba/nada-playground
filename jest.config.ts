abbbb3053896a80df10fb6b71f7c30e16bc06840
Bolarinwa Owuogba <bhorlarinwah@gmail.com>
2024-08-17 09:27:52 +0100

    basic testing w/ jest setup

---
+++ jest.config.ts
@@ -0,0 +1,26 @@
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { compilerOptions } = require("./tsconfig");
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+const jestConfig = {
+  roots: ["./src"],
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths ?? {}),
+  transform: {
+    ".+\\.(css|less|sass|scss|png|jpg|gif|ttf|woff|woff2|svg)$":
+      "jest-transform-stub",
+    ".+\\.(tsx|ts)$": [
+      "ts-jest",
+      {
+        tsconfig: {
+          jsx: "react-jsx",
+          esModuleInterop: true,
+        },
+      },
+    ],
+  },
+};
+
+export default jestConfig;
---
+++ src/components/code-editor/index.test.ts
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+// import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
+import CodeEditor from "./index";
+
+test("allows users type code", async () => {
+  console.log("works?");
+});
